a66d239ef009bb11fa3cd053ae0c494903f8f7f5
Jonathan Huergo <jh2000@hotmail.com.ar>
2022-07-05 14:22:21 -0300

    Updates

---
+++ precache-manifest.cab0e278b0ad746c335bc7cfd97ac6bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "40a612d65d2df53b52eb",
+    "url": "/static/css/main.9fa5c9e3.chunk.css"
+  },
+  {
+    "revision": "40a612d65d2df53b52eb",
+    "url": "/static/js/main.2f193706.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "316cc844a04bf0ef5f9d",
+    "url": "/static/js/2.926424c2.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "ea609580584898c7c22c19d6b372bca0",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "jhuergo.com/precache-manifest.15e8c12f6b17f67597756dce15bd5f82.js"
+  "/precache-manifest.cab0e278b0ad746c335bc7cfd97ac6bc.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("jhuergo.com/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
