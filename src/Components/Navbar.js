e3c7542559c36ad50c3d1c069bf485c15c11e877
Jonathan Huergo <jh2000@hotmail.com.ar>
2022-07-06 12:48:22 -0300

    Add Navbar

---
+++ src/Components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from '../logo.svg';
+import '../Navbar.css';
+
+function Navbar() {
+    return (
+      <nav className="navbar">
+        <ul className="nav-logo-element"><img src={logo} className="nav-logo" alt="logo" /> <img src={logo} className="nav-logo" alt="logo" /></ul>
+        <ul className="nav-element">Acerca de mi</ul>
+        <ul className="nav-element">Contacto</ul>
+      </nav>
+    );
+  }
+  
+  export default Navbar;
