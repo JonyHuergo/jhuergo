6c7455cc3afa12e95c0c2e41020c6b31816a305f
Jonathan Huergo <jh2000@hotmail.com.ar>
2022-07-11 17:58:03 -0300

    Fix Navbar not expanding on small devices

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
 import './css/index.css';
 import './css/App.css';
 import './css/Navbar.css';
