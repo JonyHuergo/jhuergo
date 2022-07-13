14d69358993f29a77f71749ed85846d2b05c4cc4
Jonathan Huergo <jh2000@hotmail.com.ar>
2022-07-13 12:56:57 -0300

    Add placeholders to about section

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { Animator, ScrollContainer, ScrollPage, batch, Fade, FadeIn, FadeOut, Move, MoveIn, MoveOut, Sticky, StickyIn, StickyOut, Zoom, ZoomIn, ZoomOut } from "react-scroll-motion";
-import logo from '../assets/logoReact.svg';
+import photo from '../assets/male-placeholder-image.jpeg';
 
 function About() {
   return (
     <ScrollPage>
       <div id="about" className="about">
         <Animator animation={batch(Fade(), MoveIn(-200, 0), MoveOut(0, -200))}>
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Acerca de mi
-          </p>
+          <h2>ACERCA DE MI</h2>
+          <div class="row align-items-center">
+            <div class="col-lg-6">
+              <h3>Titulo</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+            <div class="col-lg-6">
+              <img src={photo} className="photo" alt="photo" />
+            </div>
+          </div>
         </Animator>
       </div>
     </ScrollPage>
